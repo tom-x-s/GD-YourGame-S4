bf04e67471ad90bccad989c0584c6d462b3a372f
Tom <txsomhorst@gmail.com>
2019-02-14 14:58:19 +0100

    HealthBar PoC finished
    Finished building a PoC for the Boss' healthbar we want in our game.

---
+++ Demon Snares/Assets/Scripts/HealthBar/BarScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BarScript : MonoBehaviour
+{
+    private float fillAmount;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    [SerializeField]
+    private Image fill;
+
+    [SerializeField]
+    private Text valueText;
+
+    public float MaxValue { get; set; }
+
+    public float Value {
+        set
+        {
+            string[] tmp = valueText.text.Split(':');
+            valueText.text = tmp[0] + ": " + value;
+            fillAmount = MapHealth(value,0,MaxValue,0,1);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        HandleBar();
+    }
+
+    private void HandleBar()
+    {
+        // updates fill amount whenever it changes
+        if (fillAmount != fill.fillAmount)
+        {
+            fill.fillAmount = Mathf.Lerp(fill.fillAmount, fillAmount, Time.deltaTime * lerpSpeed);
+        }
+    }
+
+    private float MapHealth(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        // maps the actual health value to a value that the fillamount can understand
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
---
+++ Demon Snares/Assets/Scripts/HealthBar/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Stat health;
+
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            health.CurrentVal += 10;
+        }
+    }
+}
