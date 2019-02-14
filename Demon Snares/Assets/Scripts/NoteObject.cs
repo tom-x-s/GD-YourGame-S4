c99889fb9011312dfce8640a1a65d0a7767d6a9b
Dennis Vulders <dennisvulders@hotmail.com>
2019-02-14 16:17:45 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Demon Snares/Assets/Scripts/NoteObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+
+    public bool canBePressed;
+
+    public KeyCode keyToPress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(keyToPress))
+        {
+            if(canBePressed)
+            {
+                gameObject.SetActive(false);
+
+                GameManager.instance.NoteHit();
+            }
+            else //First note is always missed for some reason.
+            {
+                GameManager.instance.NoteMissed();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+
+            GameManager.instance.NoteMissed();
+        }
+    }
+}
