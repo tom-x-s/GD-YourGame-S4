c229898608d3a6d3d0aabe245500f055325d305f
Dennis Vulders <dennisvulders@hotmail.com>
2019-03-01 09:10:28 +0100

    Menu +  cursor

---
+++ Demon Snares/Assets/Scripts/ButtonMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonMenu : MonoBehaviour
+{
+    public class Bouton123 : MonoBehaviour
+    {
+        public Button button;
+        public Sprite defaultImage;
+        public Sprite pressedImage;
+
+        public KeyCode keyToPress;
+
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(keyToPress))
+            {
+                button.GetComponent<Image>().sprite = pressedImage;
+            }
+
+            if (Input.GetKeyUp(keyToPress))
+            {
+                button.GetComponent<Image>().sprite = defaultImage;
+            }
+        }
+    }
+}
---
+++ Demon Snares/Assets/Scripts/ImageChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageChange : MonoBehaviour
+{
+    public Sprite originalSprite;
+    public Sprite pressedSprite;
+
+    Image myImageComponent;
+
+    public KeyCode keyToPress;
+
+    void Start() //Lets start by getting a reference to our image component.
+    {
+        myImageComponent = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            myImageComponent.sprite = pressedSprite;
+        }
+        if (Input.GetKeyUp(keyToPress))
+        {
+            myImageComponent.sprite = originalSprite;
+        }
+    }
+}
