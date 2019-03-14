b4a3d0caeb7c6412be8ed0b2dfbd0dedbe61b856
Tom-X-S <txsomhorst@gmail.com>
2019-03-14 11:56:30 +0100

    Particles
    Added particles whenever a note is hit successfully.

--- Demon Snares/Assets/Scripts/MainMenu.cs
+++ Demon Snares/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Buttons-tutorial");
+        SceneManager.LoadScene("TomTestScene");
     }
 
     public void QuitGame()
