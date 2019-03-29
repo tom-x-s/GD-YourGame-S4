ddd4e2ae81ff45218f396455472d292f3e7afbc6
Tom-X-S <txsomhorst@gmail.com>
2019-03-29 11:39:37 +0100

    Reworked hitting & missing
    Re coded hitting and missing notes and added penalties for spamming.

--- Demon Snares/Assets/Scripts/3D/NoteObject3D.cs
+++ Demon Snares/Assets/Scripts/3D/NoteObject3D.cs
@@ -4,19 +4,8 @@ using UnityEngine;
 
 public class NoteObject3D : MonoBehaviour
 {
-    private bool canBePressed;
-
-    public KeyCode keyToPress;
-
-    private GameObject hitbox;
-    private ParticleSystem Emitter;
-
-    //public float spamTime;
-    //public bool spamAvailable;
-
     void Start()
     {
-        //spamAvailable = true;
     }
 
     void Update()
@@ -31,55 +20,6 @@ public class NoteObject3D : MonoBehaviour
                 if (Input.GetKeyDown(code)) { print(System.Enum.GetName(typeof(KeyCode), code)); }
             }
         }
-        */
-
-        if (Input.GetKeyDown(keyToPress))
-        {
-            if (canBePressed)
-            {
-                gameObject.SetActive(false);
-
-                canBePressed = false;
-
-                GameManager.instance.NoteHit();
-                Emitter.Play();
-            }
-
-            //if (!canBePressed && spamAvailable)
-            //{
-            //    spamAvailable = false;
-            //    spamTime = Time.time;
-            //    GameManager.instance.HitNothing();
-            //}
-        }
-
-        //if (spamTime <= Time.time - 0.5)
-        //{
-        //    spamAvailable = true;
-        //}
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        hitbox = other.gameObject;
-        Emitter = hitbox.GetComponentInChildren<ParticleSystem>();
-
-        if (other.tag == "Activator")
-        {
-            canBePressed = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Activator")
-        {
-            canBePressed = false;
-
-            GameManager.instance.NoteMissed();
-
-            gameObject.SetActive(false);
-
-        }
+        */  
     }
 }
