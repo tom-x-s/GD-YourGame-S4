724a4d1b6d958a0075f29d981b6ad36fa0b33a74
Dennis Vulders <dennisvulders@hotmail.com>
2019-04-12 10:48:25 +0200

    Characters in songchart
    Added vampire + variation of warroks and vampire

---
+++ Demon Snares/Assets/Scripts/ButtonNavigation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonNavigation : MonoBehaviour
+{
+
+    public string key;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            
+        }
+
+    }
+}
